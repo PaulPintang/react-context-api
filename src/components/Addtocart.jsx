82c12796ccde2b9b14e3cc40104be7bcb5ba27bb
PaulPintang <pauljpintang@gmail.com>
2022-04-14 10:51:23 +0800

    remove postcss config and install mantine

--- src/components/Addtocart.jsx
+++ src/components/Addtocart.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { Button, Checkbox } from "@mantine/core";
+import { Button } from "@mantine/core";
 const Addtocart = () => {
-  return (
-    <div className="flex justify-center">
-      <Button variant="outline" color="violet">
-        Hello world!
-      </Button>
-      <Checkbox label="I agree to sell my privacy" />
-    </div>
-  );
+  return <Button color="indigo">Add item</Button>;
 };
 
 export default Addtocart;
