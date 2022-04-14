458096cbe7144008f8f1cbf07bf318103d936bd2
PaulPintang <pauljpintang@gmail.com>
2022-04-14 11:33:09 +0800

    add conditional rendering

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,18 @@ const Cart = () => {
   const { item } = useContext(CartContext);
   return (
     <div>
-      <h1>You have {item} in your cart</h1>
+      <div className="py-6 ">
+        <i class="fa-solid fa-cart-shopping text-5xl text-gray-600 relative ">
+          <span className="rounded-full bg-green-400 text-white text-[12px] p-[8px] absolute right-[-12px] top-[-10px]">
+            {item}
+          </span>
+        </i>
+      </div>
+      {item === 0 ? (
+        <span>Your cart is empty!!</span>
+      ) : (
+        <span>You have {item} items in your cart !!</span>
+      )}
     </div>
   );
 };
