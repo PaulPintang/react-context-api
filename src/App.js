4a3776db131fa99f4f5751978be864069aa316be
PaulPintang <pauljpintang@gmail.com>
2022-04-14 10:58:28 +0800

    uninstall postcss and autoprixer / conflict with mantine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Container, MantineProvider } from "@mantine/core";
 import Addtocart from "./components/Addtocart";
 import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <Addtocart />
-      <Cart />
-    </div>
+    <MantineProvider emotionOptions={{ key: "mantine", prepend: false }}>
+      <Container className="text-center py-10">
+        <div className="App">
+          <Addtocart />
+          <Cart />
+        </div>
+      </Container>
+    </MantineProvider>
   );
 }
 
--- src/components/Addtocart.jsx
+++ src/components/Addtocart.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Button } from "@mantine/core";
 const Addtocart = () => {
-  return <Button color="indigo">Add item</Button>;
+  return (
+    <Button color="indigo" size="xs">
+      Add item
+    </Button>
+  );
 };
 
 export default Addtocart;
